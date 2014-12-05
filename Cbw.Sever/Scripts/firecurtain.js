d527291c3e375a110747cbc26ff91ff255d3d3bb
Yi Ding <yi.ding.cn@outlook.com>
2014-12-05 16:31:08 +0800

    Add front end for mode

--- Cbw.Sever/Scripts/firecurtain.js
+++ Cbw.Sever/Scripts/firecurtain.js
@@ -1,7 +1,7 @@
 ﻿function submitted() {
     var inputBox = document.getElementById("message");
-
     var message = inputBox.value;
+    var mode = 
 
     $.ajax({
         url: "http://localhost:3338/cbw/Channels(0)/Captions",
@@ -14,7 +14,9 @@
             inputBox.setAttribute('disabled', 'true');
             inputBox.value = "发送成功！下一条要等十秒钟才能发哦~";
 
-            setTimeout("document.getElementById('message').removeAttribute('disabled')", 10000);
+            setTimeout("document.getElementById('message').removeAttribute('disabled');" +
+                "document.getElementById('message').value = '';" +
+                "document.getElementById('message').setAttribute('placeholder', '吐槽吧，骚年！\\(^-^)/')", 10000);
         },
         error: function() {
             alert("出错啦~");

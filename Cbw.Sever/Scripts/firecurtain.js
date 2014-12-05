ef8d8f59564ef7a425b6f370bce205fa47449150
Yi Ding <yiding@microsoft.com>
2014-12-05 20:36:47 +0800

    Remove submit button; optimize failure logic

--- Cbw.Sever/Scripts/firecurtain.js
+++ Cbw.Sever/Scripts/firecurtain.js
@@ -1,33 +1,37 @@
-﻿function submitted() {
+﻿function keypress(event) {
+    if (event.keyCode == 13) {
+        submitted();
+    }
+}
+
+function submitted() {
     var inputBox = document.getElementById("message");
     var message = inputBox.value;
     var mode = $("input[type='radio']:checked").attr("value");
-    var submitButton = document.getElementById("submitButton");
 
-    message = "{Text:\"" + message + "\", Config:{Display: \"" + mode + "\"}}";
+    var payload = "{Text:\"" + message + "\", Config:{Display: \"" + mode + "\"}}";
 
     inputBox.setAttribute('disabled', 'true');
-    submitButton.setAttribute('disabled', 'true');
 
     $.ajax({
-        url: "cbw/Channels(0)/Captions",
+        url: "cbw/Channels(0)/Caption",
         type: "POST",
-        data: message,
+        data: payload,
         headers: {
             "Content-Type": "application/json"
         },
-        success: function() {
+        success: function () {
             inputBox.value = "发送成功！下一条要等三秒钟才能发哦~";
 
             setTimeout("document.getElementById('message').removeAttribute('disabled');" +
-                "document.getElementById('submitButton').removeAttribute('disabled');" + 
                 "document.getElementById('message').value = '';" +
                 "document.getElementById('message').setAttribute('placeholder', '吐槽吧，骚年！\\(^-^)/');", 3000);
         },
         error: function () {
-            document.getElementById('message').removeAttribute('disabled');
-            document.getElementById('submitButton').removeAttribute('disabled');
-            alert("出错啦~");
+            inputBox.value = "发送失败了T_T。。没事，等三秒钟再试试~";
+
+            setTimeout("document.getElementById('message').removeAttribute('disabled');" +
+                "document.getElementById('message').value = '" + message + "';", 3000);
         }
     });
 }

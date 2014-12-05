78130b4dd6d3580084dd523a73a41eb48582cd89
Yi Ding <yi.ding.cn@outlook.com>
2014-12-05 16:56:28 +0800

    Add mode choosing backend and fix bug

--- Cbw.Sever/Scripts/firecurtain.js
+++ Cbw.Sever/Scripts/firecurtain.js
@@ -1,24 +1,32 @@
 ﻿function submitted() {
     var inputBox = document.getElementById("message");
     var message = inputBox.value;
-    var mode = 
+    var mode = $("input[type='radio']:checked").attr("value");
+    var submitButton = document.getElementById("submitButton");
+
+    message = "{Text:\"" + message + "\", Config:{Display: \"" + mode + "\"}}";
+
+    inputBox.setAttribute('disabled', 'true');
+    submitButton.setAttribute('disabled', 'true');
 
     $.ajax({
-        url: "http://localhost:3338/cbw/Channels(0)/Captions",
+        url: "cbw/Channels(0)/Captions",
         type: "POST",
-        data: "{Text:\"" + message + "\"}",
+        data: message,
         headers: {
             "Content-Type": "application/json"
         },
         success: function() {
-            inputBox.setAttribute('disabled', 'true');
-            inputBox.value = "发送成功！下一条要等十秒钟才能发哦~";
+            inputBox.value = "发送成功！下一条要等三秒钟才能发哦~";
 
             setTimeout("document.getElementById('message').removeAttribute('disabled');" +
+                "document.getElementById('submitButton').removeAttribute('disabled');" + 
                 "document.getElementById('message').value = '';" +
-                "document.getElementById('message').setAttribute('placeholder', '吐槽吧，骚年！\\(^-^)/')", 10000);
+                "document.getElementById('message').setAttribute('placeholder', '吐槽吧，骚年！\\(^-^)/');", 3000);
         },
-        error: function() {
+        error: function () {
+            document.getElementById('message').removeAttribute('disabled');
+            document.getElementById('submitButton').removeAttribute('disabled');
             alert("出错啦~");
         }
     });
